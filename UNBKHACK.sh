f7b19212c9f7e3b49a1640743aa7e8210bbb6966
GilangKun <49320620+GilangKun@users.noreply.github.com>
2019-04-24 09:31:33 +0700

    Create UNBKHACK.sh

---
+++ UNBKHACK.sh
@@ -0,0 +1,194 @@
+#!/usr/bin/bash
+#tools sederhana
+#cheat unbk
+#by GilangKun
+clear
+
+b='\033[34;1m'
+g='\033[32;1m'
+p='\033[35;1m'
+c='\033[36;1m'
+r='\033[31;1m'
+
+
+echo " "
+echo  $b "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo  $r "■               $g HACK UNBK                  $r ■"
+echo  $r "■                                              $r ■"
+echo  $r "■ $b Author      $c : $g LxG_9                 $r ■"
+echo  $r "■ $b Contact     $c : $g 081226988596          $r ■"
+echo  $r "■ $b Thanks To   $c : $g Mr.Devil              $r ■"
+echo  $r "■     $g TEAM KILLER CYBER TEAM                $r ■"
+echo  $b "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+sleep 1
+echo " "
+echo " "
+echo $r "╭╼━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╾╮"
+echo $r "│             $g MENU SOAL         $r │"
+echo $r "│                                  $r │"
+echo $r "│ $p 01. $b MATEMATIKA             $r │"      
+echo $r "│ $c 02. $g ILMU PENGETAHUAN ALAM  $r │"
+echo $r "│ $b 03. $b BAHASA INDONESIA       $r │"
+echo $r "│ $g 04. $c BAHASA INGGRIS         $r │"
+echo $r "│ $p 00. $b EXIT                   $r │"            
+echo $r "│                                  $r │"
+echo $r "╰╼━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╼╯"
+echo " "
+echo $b"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@GilangKun=•>" gans;
+echo " "
+echo " "
+
+
+if [ $gans = 1 ] || [ $gans = 01 ]
+then 
+echo  $p "[Masukkan User]>"
+read -p "───────root@user=•>" us;
+sleep 2
+echo " "
+echo  $g "[Masukkan Pasword]>"
+read -p "───────root@pasword=•>" pw;
+echo " "
+sleep 2
+echo  $c "WAITING PROSES 40 SECOND"
+sleep 40
+echo  $b "╭╼━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╾╮"
+echo  $b "│             $g MATEMATIKA           $b │"
+echo  $b "│                                        │"
+echo  $b "│ $p 01.c   11.d   21.a   31.e   41.  $b │"
+echo  $b "│ $p 02.a   12.a   22.c   32.a   42.  $b │"
+echo  $b "│ $p 03.c   13.d   23.b   33.c   43.  $b │"
+echo  $b "│ $p 04.b   14.c   24.d   34.b   44.  $b │"
+echo  $b "│ $p 05.d   15.b   25.a   35.a   45.  $b │" 
+echo  $b "│ $g 06.e   16.a   26.c   36.c   46.  $b │"
+echo  $b "│ $g 07.b   17.e   27.e   37.c   47.  $b │"
+echo  $b "│ $g 08.c   18.b   28.d   38.d   48.  $b │"
+echo  $b "│ $g 09.a   19.c   29.a   39.a   49.  $b │"
+echo  $b "│ $g 10.d   20.a   30.e   40.d   50.  $b │"
+echo  $b "│                                        │"
+echo  $b "╰╼━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╼╯"
+sleep 1
+echo $p "JAWABAN BERHASIL DIBOBOL"
+sleep 2
+echo $g "Jangan Beritahu Guru Anda"
+sleep 2
+echo $b "Agar Tidak Kena Hukum"
+fi
+
+
+if [ $gans = 2 ] || [ $gans = 02 ]
+then 
+echo  $p "Masukkan User"
+read -p "───────root@user=•>" us;
+sleep 2
+echo " "
+echo  $g "Masukkan Pasword"
+read -p "───────root@pasword=•>" pw;
+echo " "
+sleep 2
+echo  $c "WAITING PROSES 40 SECOND"
+sleep 40
+echo  $b "╭╼━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╾╮"
+echo  $b "│      $g ILMU PENGETAHUAN ALAM       $b │"
+echo  $b "│                                        │"
+echo  $b "│ $p 01.c   11.d   21.a   31.e   41.  $b │"
+echo  $b "│ $p 02.a   12.a   22.c   32.a   42.  $b │"
+echo  $b "│ $p 03.c   13.d   23.b   33.c   43.  $b │"
+echo  $b "│ $p 04.b   14.c   24.d   34.b   44.  $b │"
+echo  $b "│ $p 05.d   15.b   25.a   35.a   45.  $b │" 
+echo  $b "│ $g 06.e   16.a   26.c   36.c   46.  $b │"
+echo  $b "│ $g 07.b   17.e   27.e   37.c   47.  $b │"
+echo  $b "│ $g 08.c   18.b   28.d   38.d   48.  $b │"
+echo  $b "│ $g 09.a   19.c   29.a   39.a   49.  $b │"
+echo  $b "│ $g 10.d   20.a   30.e   40.d   50.  $b │"
+echo  $b "│                                        │"
+echo  $b "╰╼━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╼╯"
+sleep 1
+echo $p "JAWABAN BERHASIL DIBOBOL"
+sleep 2
+echo $g "Jangan Beritahu Guru Anda"
+sleep 2
+echo $b "Agar Anda Tidak Kena Hukum" 
+fi
+
+
+if [ $gans = 3 ] || [ $gans = 03 ]
+then 
+echo  $p "Masukkan User"
+read -p "───────root@user=•>" us;
+sleep 2
+echo " "
+echo  $g "Masukkan Pasword"
+read -p "───────root@pasword=•>" pw;
+echo " "
+sleep 2
+echo  $c "WAITING PROSES 40 SECOND"
+sleep 40
+echo  $b "╭╼━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╾╮"
+echo  $b "│        $g BAHASA INDONESIA           $b │"
+echo  $b "│                                         │"
+echo  $b "│ $p 01.c   11.d   21.a   31.e   41.c  $b │"
+echo  $b "│ $p 02.a   12.a   22.c   32.a   42.a  $b │"
+echo  $b "│ $p 03.c   13.d   23.b   33.c   43.b  $b │"
+echo  $b "│ $p 04.b   14.c   24.d   34.b   44.c  $b │"
+echo  $b "│ $p 05.d   15.b   25.a   35.a   45.d  $b │" 
+echo  $b "│ $g 06.e   16.a   26.c   36.c   46.e  $b │"
+echo  $b "│ $g 07.b   17.e   27.e   37.c   47.b  $b │"
+echo  $b "│ $g 08.c   18.b   28.d   38.d   48.d  $b │"
+echo  $b "│ $g 09.a   19.c   29.a   39.a   49.a  $b │"
+echo  $b "│ $g 10.d   20.a   30.e   40.d   50.d  $b │"
+echo  $b "│                                         │"
+echo  $b "╰╼━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╼╯"
+sleep 1
+echo $p "JAWABAN BERHASIL DIBOBOL"
+sleep 2
+echo $g "Jangan Beritahu Guru Anda"
+sleep 2
+echo $b "Agar Anda Tidak Kena Hukum"
+fi
+
+
+if [ $gans = 4 ] || [ $gans = 04 ]
+then 
+echo  $p "Masukkan User"
+read -p "───────root@user=•>" us;
+sleep 2
+echo " "
+echo  $g "Masukkan Pasword"
+read -p "───────root@pasword=•>" pw;
+echo " "
+sleep 2
+echo  $c "WAITING PROSES 40 SECOND"
+sleep 40
+echo  $b "╭╼━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╾╮"
+echo  $b "│          $g BAHASA INGGRIS          $b │"
+echo  $b "│                                        │"
+echo  $b "│ $p 01.c   11.d   21.a   31.e   41.a $b │"
+echo  $b "│ $p 02.a   12.a   22.c   32.a   42.e $b │"
+echo  $b "│ $p 03.c   13.d   23.b   33.c   43.d $b │"
+echo  $b "│ $p 04.b   14.c   24.d   34.b   44.a $b │"
+echo  $b "│ $p 05.d   15.b   25.a   35.a   45.b $b │" 
+echo  $b "│ $g 06.e   16.a   26.c   36.c   46.c $b │"
+echo  $b "│ $g 07.b   17.e   27.e   37.c   47.d $b │"
+echo  $b "│ $g 08.c   18.b   28.d   38.d   48.b $b │"
+echo  $b "│ $g 09.a   19.c   29.a   39.a   49.a $b │"
+echo  $b "│ $g 10.d   20.a   30.e   40.d   50.d $b │"
+echo  $b "│                                        │"
+echo  $b "╰╼━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╼╯"
+sleep 1
+echo $p "JAWABAN BERHASIL DIBOBOL"
+sleep 2
+echo $g "Jangan Beritahu Guru Anda"
+sleep 2
+echo $b "Agar Anda Tidak Kena Hukum"
+fi
+
+
+if [ $gans = 0 ] || [ $gans = 00 ]
+then 
+echo $p "GUNAKAN TOOLS INI DENGAN BAIK"
+sleep 2
+echo $r "GILANG KUN"
+exit 
+fi
+    
